1ce3eb1fa6c1c27848b0d7c46607f08f2b31ed0b
Mahadi Hasan <mahadi250721@gmail.com>
2021-08-03 22:55:55 +0600

    checkbox-made

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let records = document.getElementById("showRecords");
-let text = document.getElementById("inputText").value;
+const input = document.getElementById("inputText");
+const container = document.getElementById("record-container");
+
+let count = 0;
+let records = [];
+
 
 function addRecord(){
+    if (input.value != null){  //has issue with empty string
+        records[count] = addingCheckbox();
+        count++;
+    }
+
+    container.appendChild(records[count-1]);
+    records[count-1].addEventListener("click",cutTheSentence)
+}
+
+
+function addingCheckbox() {
+    let newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.id = "id" + count;
+    newCheckbox.style.marginRight = "10px";
+
+    return newCheckbox;
+}
+
+function cutTheSentence() {
+    //after clicking the checkbox the sentence will be cutted.
 }
