efebeb68bc8162da98f63636da29539e44d19ff4
Mahadi Hasan <mahadi250721@gmail.com>
2021-08-05 15:41:18 +0600

    checkbox-todoNote-added

--- index.js
+++ index.js
@@ -6,16 +6,44 @@ let records = [];
 
 
 function addRecord(){
-    if (input.value != null){  //has issue with empty string
-        records[count] = addingCheckbox();
-        count++;
-    }
-
-    container.appendChild(records[count-1]);
+    container.appendChild(internalConatiner())   //hold checkbox and the 
 }
 
 
-function addingCheckbox() {
+function internalConatiner(){
+    let div = document.createElement('DIV');
+    styleInternalConatainer(div);
+
+    div.appendChild(checkbox());
+    div.appendChild(to_doNote());
+
+    return div;
+}
+
+
+function styleInternalConatainer(div){
+    let divStyle = div.style;
+
+    divStyle.width = "525px";
+    divStyle.height = "50px";
+    divStyle.border = "2px solid grey";
+    divStyle.borderRadius = "5px";
+    divStyle.marginBottom = "10px";
+    divStyle.display = "flex";
+    divStyle.alignItems = "center";
+}
+
+
+function to_doNote(){
+    let paragraph = document.createElement('P');
+    paragraph.textContent = input.value;
+
+    return paragraph;
+}
+
+
+
+function checkbox() {
     let newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
     newCheckbox.id = "id" + count;
